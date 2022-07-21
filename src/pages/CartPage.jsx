844fe16902e0fe9edeb3b4d3635f008d3fac4443
kon010101 <kornisemail@gmail.com>
2022-07-21 10:33:03 +0200

    Add Cart Page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useContext } from "react";
+import { Button, Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+
+import CardGrid from "../components/CardGrid/CardGrid";
+import { EventContext } from "../context/EventContext";
+import { eventActionTypes } from "../context/actionTypes";
+
+const { CLEAR_CART } = eventActionTypes;
 
 const CartPage = () => {
-  return <div>CartPage</div>;
+  const eventContext = useContext(EventContext);
+  const { eventState, dispatch } = eventContext;
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
+
+  return (
+    <div>
+      <Container>
+        <Typography
+          sx={{
+            margin: "1rem 0",
+            paddingTop: "64px",
+          }}
+          variant="h2"
+        >
+          Cart
+        </Typography>
+        <Button onClick={clearCart}>Clear Cart</Button>
+        {eventState.cartEvents.length > 0 ? (
+          <CardGrid events={eventState.cartEvents} />
+        ) : (
+          <Box>Cart empty...</Box>
+        )}
+      </Container>
+    </div>
+  );
 };
 
 export default CartPage;
