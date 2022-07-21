07e7966b6f3784cb97a54bca21946e7ed9385fea
kon010101 <kornisemail@gmail.com>
2022-07-21 09:59:38 +0200

    Add router and context to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+
+import LandingPage from "./pages/LandingPage";
+import CartPage from "./pages/CartPage";
+import { EventContextProvider } from "./context/EventContext";
+
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <EventContextProvider>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="cart" element={<CartPage />} />
+          </Routes>
+        </EventContextProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
