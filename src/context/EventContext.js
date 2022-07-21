5f927a47bc3ffe4c732d66fca3c032a40cb8bdd6
kon010101 <kornisemail@gmail.com>
2022-07-21 09:50:40 +0200

    Add context

---
+++ src/context/EventContext.js
@@ -0,0 +1,80 @@
+import { createContext, useReducer } from "react";
+import { eventActionTypes } from "./actionTypes.js";
+
+const {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CLEAR_CART,
+  FETCH_SUCCESS,
+  FETCH_ERROR,
+  SET_FILTERED,
+} = eventActionTypes;
+
+const initialState = {
+  allEvents: [],
+  filteredEvents: [],
+  cartEvents: [],
+  loading: true,
+  error: "",
+};
+
+const eventReducer = (state, action) => {
+  const { filteredEvents, cartEvents } = state;
+
+  switch (action.type) {
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        allEvents: action.payload,
+        loading: false,
+      };
+    case FETCH_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    case SET_FILTERED:
+      return {
+        ...state,
+        filteredEvents: action.payload,
+      };
+    case ADD_TO_CART:
+      if (cartEvents.find((item) => action.payload === item)) {
+        return state;
+      }
+      return {
+        ...state,
+        cartEvents: [...cartEvents, action.payload],
+        filteredEvents: filteredEvents.filter(
+          (event) => action.payload !== event
+        ),
+      };
+    case REMOVE_FROM_CART:
+      return {
+        ...state,
+        cartEvents: cartEvents.filter(
+          (item) => item._id !== action.payload._id
+        ),
+      };
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartEvents: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export const EventContext = createContext();
+
+export const EventContextProvider = ({ children }) => {
+  const [eventState, dispatch] = useReducer(eventReducer, initialState);
+
+  return (
+    <EventContext.Provider value={{ eventState, dispatch }}>
+      {children}
+    </EventContext.Provider>
+  );
+};
